439be375f841dfd9174f4db4487eb83f72e76f8e
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2021-01-13 02:03:35 +0100

    changed name

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Pages/Home";
 import Contact from "./components/Pages/Contact";
 import About from "./components/Pages/About";
 import Header from "./components/Pages/Header";
-import Menu from "./components/Pages/Menu/Menu";
+import PizzaMenu from "./components/Pages/Menu/PizzaMenu";
 import Galery from "./components/Pages/Galery";
 import Cart from "./components/Pages/Cart";
 import Admin from "./components/Admin/Admin";
@@ -21,7 +21,7 @@ export default function App() {
         <Route exact from="/" render={props => <Home {...props} />} />
         <Route exact path="/contact" render={props => <Contact {...props} />} />
         <Route exact path="/about" render={props => <About {...props} />} />
-        <Route exact path="/menu" render={props => <Menu {...props} />} />
+        <Route exact path="/menu" render={props => <PizzaMenu {...props} />} />
         <Route exact path="/galery" render={props => <Galery {...props} />} />
         <Route exact path="/cart" render={props => <Cart {...props} />} />
         <Route exact path="/admin" render={props => <Admin {...props} />} />
