ddb219679dfbce1d2c721224a29fd8597aa980af
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2021-01-15 16:04:51 +0100

    router test18

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <CssBaseline />
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
