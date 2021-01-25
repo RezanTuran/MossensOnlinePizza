1912ae70542fbd1705cba8d250ff18596e4db88d
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2021-01-25 03:14:42 +0100

    cookies bug is fixed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["https://mossenspizzeria.herokuapp.com"],
+    origin: ["https://mossenspizzeria.herokuapp.com/#/"],
     methods: ["GET", "POST"],
     credentials: true
 }));
