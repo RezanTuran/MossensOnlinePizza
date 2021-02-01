85a4f90099b062d1d480e3a41221019c0ea5eac2
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2021-02-01 18:31:10 +0100

    customer information

--- client/src/components/Admin/Order.js
+++ client/src/components/Admin/Order.js
@@ -11,21 +11,29 @@ function Order() {
         })
     }, [])
 
-    
+
     return (
         <div>
             {orderList.map((val) => {
                 return (
                     <div className="card">
-                        <h2>Kund: {val.firstName} {val.sureName}</h2>
+                        <h2 style={{textAlign:'center'}}>Kunduppgifter</h2>
+                        <h4>Namn: <span style={{ color: 'yellow' }}>{val.firstName} {val.sureName}</span></h4>
+                        <h4>Address: <span style={{ color: 'yellow' }}>{val.adress}</span></h4>
+                        <h4>Postnummer: <span style={{ color: 'yellow' }}>{val.postNumber}</span></h4>
+                        <h4>Telefonnummer: <span style={{ color: 'yellow' }}>{val.phone}</span></h4>
+                        <h4>E-post: <span style={{ color: 'yellow' }}>{val.epost}</span></h4>
+                        <h4>Datum: <span style={{ color: 'yellow' }}>{val.date}</span></h4>
+                        <h2 style={{textAlign:'center'}}>Beställning</h2>
                         <hr></hr>
-                        <p>Pizzanamn: <span style={{color: 'yellow'}}>{val.pizzaName}</span></p>
+                        <p>Pizzanamn: <span style={{ color: 'yellow' }}>{val.pizzaName}</span></p>
                         <hr></hr>
-                        <p>Antal pizza: <span style={{color: 'yellow'}}>{val.quantity}</span></p>
+                        <p>Antal pizza: <span style={{ color: 'yellow' }}>{val.quantity}</span></p>
                         <hr></hr>
-                        <p>Ingredienser: <span style={{color: 'yellow'}}>{val.ingredients}</span></p>
+                        <p>Ingredienser: <span style={{ color: 'yellow' }}>{val.ingredients}</span></p>
                         <hr></hr>
-                        <p>Totalpris: <span style={{color: 'yellow'}}>{val.pizzaPrice}</span></p>
+                        <p>Totalpris: <span style={{ color: 'yellow' }}>{val.pizzaPrice} :-</span></p>
+                        <hr></hr>
                     </div>
                 )
             })}
