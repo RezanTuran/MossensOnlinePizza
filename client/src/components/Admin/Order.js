cb7eb426b10571367b827afbd7d42bd06ed3547c
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2021-02-02 01:54:43 +0100

    orderPage frontend fixed

--- client/src/components/Admin/Order.js
+++ client/src/components/Admin/Order.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function Order() {
 
@@ -21,24 +23,31 @@ function Order() {
             {orderList.map((val) => {
                 return (
                     <div className="card">
-                        <h2 style={{textAlign:'center'}}>Kunduppgifter</h2>
-                        <h4>Namn: <span style={{ color: 'yellow' }}>{val.firstName} {val.sureName}</span></h4>
-                        <h4>Address: <span style={{ color: 'yellow' }}>{val.adress}</span></h4>
-                        <h4>Postnummer: <span style={{ color: 'yellow' }}>{val.postNumber}</span></h4>
-                        <h4>Telefonnummer: <span style={{ color: 'yellow' }}>{val.phone}</span></h4>
-                        <h4>E-post: <span style={{ color: 'yellow' }}>{val.epost}</span></h4>
-                        <h4>Datum: <span style={{ color: 'yellow' }}>{val.date}</span></h4>
-                        <h2 style={{textAlign:'center'}}>Beställning</h2>
+                        <h2 className="orderTitle">Kunduppgifter</h2>
+                        <h4>Namn: <span className="customerInfo">{val.firstName} {val.sureName}</span></h4>
+                        <h4>Address: <span className="customerInfo">{val.adress}</span></h4>
+                        <h4>Postnummer: <span className="customerInfo">{val.postNumber}</span></h4>
+                        <h4>Telefonnummer: <span className="customerInfo">{val.phone}</span></h4>
+                        <h4>E-post: <span className="customerInfo">{val.epost}</span></h4>
+                        <h4>Datum: <span className="customerInfo">{val.date}</span></h4>
+                        <h2 className="orderTitle">Beställning</h2>
                         <hr></hr>
-                        <p>Pizzanamn: <span style={{ color: 'yellow' }}>{val.pizzaName}</span></p>
+                        <p>Pizzanamn: <span className="customerInfo">{val.pizzaName}</span></p>
                         <hr></hr>
-                        <p>Antal pizza: <span style={{ color: 'yellow' }}>{val.quantity}</span></p>
+                        <p>Antal pizza: <span className="customerInfo">{val.quantity}</span></p>
                         <hr></hr>
-                        <p>Ingredienser: <span style={{ color: 'yellow' }}>{val.ingredients}</span></p>
+                        <p>Ingredienser: <span className="customerInfo">{val.ingredients}</span></p>
                         <hr></hr>
-                        <p>Totalpris: <span style={{ color: 'yellow' }}>{val.pizzaPrice} :-</span></p>
+                        <p>Totalpris: <span className="customerInfo">{val.pizzaPrice} :-</span></p>
                         <hr></hr>
-                        <button onClick={() => { deleteOrder(val.id) }}>Ta Bort</button>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            endIcon={<DeleteIcon />}
+                            onClick={() => { deleteOrder(val.id) }}
+                        >
+                            Ta Bort
+                        </Button>
                     </div>
                 )
             })}
